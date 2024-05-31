6d571553cbfc5740695f8666cefce270408976fd
Philip Heindel <p.e.heindel@gmail.com>
2024-05-31 08:00:39 -0400

    Added missing Orange player texture

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -5,7 +5,7 @@ const SPEED: float = 200.0
 const JUMP_VELOCITY: float = -300.0
 
 
-@export_enum("Blue", "Green", "Pink", "Yellow") var player_type: String = "Green"
+@export_enum("Blue", "Green", "Orange", "Pink", "Yellow") var player_type: String = "Green"
 @export var coin_count: int = 0
 @export var max_jump_duration: float = 0.1
 
