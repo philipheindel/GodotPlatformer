77a809d47b1acd2924a1bd743ebf2f1e6084899b
Philip Heindel <p.e.heindel@gmail.com>
2024-06-02 08:56:11 -0400

    Got the coin audio working and started tinkering with scene switching

--- items/coin/coin.gd
+++ items/coin/coin.gd
@@ -1,8 +1,17 @@
 extends AnimatedSprite2D
 
 
+var collected: bool = false
+
+
+func _physics_process(delta):
+	if collected and not $AudioStreamPlayer.playing:
+		queue_free()
+
+
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
-		$AudioStreamPlayer.autoplay
+		collected = true
+		$AudioStreamPlayer.play()
 		body.add_coin()
-		queue_free()
+		visible = false
