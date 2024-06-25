c34476a985bf116c9ce4ecd9e8d4610f788fe948
Philip Heindel <p.e.heindel@gmail.com>
2024-06-25 08:02:37 -0400

    Changed heart approach to one where all hearts are already in scene and will just be enabled/disabled as needed

--- entities/player/components/health/health.gd
+++ entities/player/components/health/health.gd
@@ -9,10 +9,10 @@ var health_heart: Resource = preload("res://entities/player/components/health/he
 
 
 func _ready():
+	
 	var sub: int = max / 2
 	for i in sub:
 		var new_heart: AnimatedSprite2D = health_heart.instantiate()
-		var abc = $Health_Heart.sprite_frames.get_frame_texture($Health_Heart.animation, $Health_Heart.frame).get_size()
 		new_heart.position.x = new_heart.position.x + 20
 		$".".add_child(new_heart)
 
