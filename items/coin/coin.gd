1edd50cca18a11f1998ffda940e938174c93d0c7
Philip Heindel <p.e.heindel@gmail.com>
2024-07-02 21:51:02 -0400

    Added collection animation to coin

--- items/coin/coin.gd
+++ items/coin/coin.gd
@@ -4,24 +4,19 @@ extends Node2D
 @export_enum("Bronze", "Silver", "Gold", "Platinum") var coin_type: String = "Bronze"
 
 
-var collected: bool = false
-
-
 func _ready() -> void:
 	$Coin.play(coin_type.to_lower())
 
 
-func _process(delta) -> void:
-	if collected and not $AudioStreamPlayer.playing:
-		queue_free()
-
-
 func _on_area_2d_body_entered(body) -> void:
 	if body.name == "Player":
-		collected = true
 		$AudioStreamPlayer.play()
+		$AnimationPlayer.play("collect")
 		body.add_coin()
-		visible = false
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	queue_free()
 
 
 func update_animation(coin_type: String) -> void:
