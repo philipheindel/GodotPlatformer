44b024781d9b22f241ee84929b8932d5de22e6aa
Philip Heindel <p.e.heindel@gmail.com>
2024-07-05 13:08:07 -0400

    Troubleshooting jump re-implementation issues

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -54,9 +54,9 @@ func _physics_process(delta: float) -> void:
 	print(is_on_floor())
 	if not is_on_floor():
 		if velocity.y > 0:
-			velocity.y -= jump_gravity * delta
+			velocity.y += jump_gravity * delta
 		else:
-			velocity.y -= fall_gravity * delta
+			velocity.y += fall_gravity * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
