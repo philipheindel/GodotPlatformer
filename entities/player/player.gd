6f4b8474e73a9cdf689757d6c51139703cfbd326
Philip Heindel <p.e.heindel@gmail.com>
2024-07-06 15:43:15 -0400

    Going insane trying to make this jumping mechanic work

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -15,7 +15,7 @@ const JUMP_VELOCITY: float = -300.0
 @export_subgroup("Jumping")
 @export var jump_peak_time: float = 0.5
 @export var jump_fall_time: float = 0.5
-@export var jump_height: float = 2.0
+@export var jump_height: float = 5.0
 @export var jump_distance: float = 4.0
 var speed: float = 5.0
 var jump_velocity: float = 5.0
@@ -38,6 +38,10 @@ func _ready() -> void:
 	$AnimatedSprite2D.play()
 	$Camera2D.set_as_top_level(true)
 	$Camera2D/Health.max = health
+	print(jump_gravity)
+	print(fall_gravity)
+	print(jump_velocity)
+	print("ready?")
 
 
 func _process(_delta) -> void:
@@ -51,27 +55,61 @@ func _process(_delta) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	print(is_on_floor())
+	print(velocity.y)
+	print(gravity)
+	print(jump_gravity)
+	print(fall_gravity)
+	print(delta)
 	if not is_on_floor():
+		#velocity.y += gravity * delta
 		if velocity.y > 0:
-			velocity.y += jump_gravity * delta
+			velocity.y -= jump_gravity * delta
 		else:
-			velocity.y += fall_gravity * delta
+			velocity.y -= fall_gravity * delta
+
+	if (Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_accept")) and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	if (Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_accept")) and not is_on_floor():
+		falling = true
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		$AnimatedSprite2D.animation = _get_animation("walking")
+		if direction == 1:
+			$AnimatedSprite2D.flip_h = true
+		elif direction == -1:
+			$AnimatedSprite2D.flip_h = false
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.animation = _get_animation("idle")
+
+
+	move_and_slide()
+	return
+	#print(velocity.y)
+	if not is_on_floor():
+		#velocity.y -= jump_gravity * delta
+		if velocity.y > 0:
+			velocity.y -= jump_gravity * delta
+		else:
+			velocity.y -= fall_gravity * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 
 
 	# https://www.youtube.com/watch?v=FvFx1R3p-aw
-	move_and_slide()
-	return
+	#move_and_slide()
+	#return
 	if (Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_accept")) and velocity.y < 0:
 		velocity.y = JUMP_VELOCITY / 4
 	
 	if (Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_accept")):
 		velocity.y = JUMP_VELOCITY
 	
-	var direction = Input.get_axis("ui_left", "ui_right")
+	#var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		$AnimatedSprite2D.animation = _get_animation("walking")
 		if direction == 1:
@@ -129,6 +167,6 @@ func _get_animation(animation: String) -> String:
 	return "{player_type}_{animation}".format({"player_type": player_type, "animation": animation}).to_lower()
 
 func _calculate_movement_params() -> void:
-	jump_gravity = (2 * jump_height) / pow(jump_peak_time, 2)
-	fall_gravity = (2 * jump_height) / pow(jump_fall_time, 2)
+	jump_gravity = (2 * (jump_height * 100)) / pow(jump_peak_time, 2)
+	fall_gravity = (2 * (jump_height * 100)) / pow(jump_fall_time, 2)
 	jump_velocity = jump_gravity * jump_peak_time
