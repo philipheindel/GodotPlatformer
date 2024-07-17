cb62327ecfdabd2aeab7fc79699772dae30f1159
Philip Heindel <p.e.heindel@gmail.com>
2024-07-16 23:12:21 -0400

    Migrated drill_head mob from CharacterBody2D to Node2D and added patrol animation

--- entities/drill_head/drill_head.gd
+++ entities/drill_head/drill_head.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends Node2D
 
 
 const SPEED = 300.0
@@ -11,19 +11,19 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta) -> void:
 	return
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if not $CharacterBody2D.is_on_floor():
+		$CharacterBody2D.velocity.y += gravity * delta
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") and $CharacterBody2D.is_on_floor():
+		$CharacterBody2D.velocity.y = JUMP_VELOCITY
 	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		$CharacterBody2D.velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$CharacterBody2D.velocity.x = move_toward($CharacterBody2D.velocity.x, 0, SPEED)
 
-	move_and_slide()
+	$CharacterBody2D.move_and_slide()
 
 
 func _on_area_2d_body_entered(body) -> void:
