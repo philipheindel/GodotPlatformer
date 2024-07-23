9bedd8759437c52e929ef99a2ba131848f88f8cb
Philip Heindel <p.e.heindel@gmail.com>
2024-07-22 20:20:26 -0400

    Added Audacity and LMMS project files for custom sounds. Also added custom gem collect sound

--- items/gem/gem.gd
+++ items/gem/gem.gd
@@ -6,6 +6,8 @@ extends Node2D
 
 
 var collected: bool = false
+var audio_done: bool = false
+var animation_done: bool = false
 
 
 func _ready() -> void:
@@ -14,14 +16,24 @@ func _ready() -> void:
 		$AnimationPlayer.play("bounce")
 
 
+func _process(_delta: float) -> void:
+	if audio_done and animation_done:
+		queue_free()
+
+
 func _on_area_2d_body_entered(body) -> void:
 	if body.name == "Player":
-		collected = true
-		#$AudioStreamPlayer.play()
-		$AnimationPlayer.play("collect")
 		body.add_coin()
+		collected = true
+		$AnimatedSprite2D/Area2D.set_process(false)
+		$AudioStreamPlayer.play()
+		$AnimationPlayer.play("collect")
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "collect":
-		queue_free()
+	if anim_name == "collect" :
+		animation_done = true
+
+
+func _on_audio_stream_player_finished():
+	audio_done = true
